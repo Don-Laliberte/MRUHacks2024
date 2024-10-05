cebbd7dcb0b2a6424f78a0fc57e636b17fd367b3
Don Laliberte <donhenrylaliberte@gmail.com>
2024-10-05 14:44:36 -0600

    Added Basic Navbar and Drawer Menu on the Right Side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
+import Box from "@mui/material/Box";
+import NavBar from "./components/navbar";
+import ClippedDrawer from "./components/DrawerMenu";
+
+
 function App() {
-  
+  return (
+    <Box>
+      <NavBar></NavBar>
+    </Box>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,145 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
+import SearchIcon from "@mui/icons-material/Search";
+import Drawer from "@mui/material/Drawer";
+import List from '@mui/material/List';
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { useState } from 'react';
+
+
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+
+
+export default function NavBar() {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(0);
+
+    const toggleDrawer = (open) => (event) => {
+        if (
+          event.type === 'keydown' &&
+          (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+          return;
+        }
+        setIsDrawerOpen(open);
+      };
+
+      const menuItems = [
+        { text: 'Home', icon: <HomeIcon /> },
+        { text: 'Profile', icon: <PersonIcon /> },
+        { text: 'Settings', icon: <SettingsIcon /> },
+      ];
+    
+    const drawerContent = (
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List>
+            {menuItems.map((item) => (
+              <ListItem button key={item.text}>
+                <ListItemIcon>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar color="purple">
+          <IconButton   
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            bProductive
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={isDrawerOpen} onClose={toggleDrawer(false)}>
+        <Toolbar />
+          {drawerContent}
+      </Drawer>
+    </Box>
+  );
+}
