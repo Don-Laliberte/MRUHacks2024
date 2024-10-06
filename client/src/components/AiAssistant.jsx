6b507f74f6cd54e54d18d2b95a649c9e56ab9fe1
Don Laliberte <donhenrylaliberte@gmail.com>
2024-10-06 10:22:12 -0600

    Ai window done

--- client/src/components/AiAssistant.jsx
+++ client/src/components/AiAssistant.jsx
@@ -1,49 +1,41 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
 import { Typography } from "@mui/material";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
-
+import ReactMarkdown from "react-markdown";
+import Box from "@mui/material/Box";
 
 export default function AiAssistant({ open, toggleAiDrawer, aiChat }) {
-
-
   return (
-    <Drawer anchor="right"  open={open} 
-    sx={{
-      width: 240, // Set your desired width
-      flexShrink: 0,
-      '& .MuiDrawer-paper': {
-        width: "25%", // Also set the width for the paper element
-        boxSizing: 'border-box',
-      },
-    }}
-    onClose={toggleAiDrawer(false)}>
-        <Toolbar />
-        <Typography
-          variant="h5"
-          gutterBottom
-          component="div"
-          sx={{ p: 2, pb: 0 }}
-        >
-          Personal Productivity Assistant
-        </Typography>
-        <List sx={{ mb: 2, width: "100%" }}>
-          {/*Placeholder Text Logic*/}
-          {aiChat && aiChat.map(({ id, primary, secondary }) => (
-            <React.Fragment key={id}>
-              {id === 1 && <ListSubheader>Today</ListSubheader>}
-              {id === 3 && <ListSubheader>Yesterday</ListSubheader>}
-              <ListItem>
-                <ListItemText primary={primary} secondary={secondary} />
-              </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
+    <Drawer
+      anchor="right"
+      open={open}
+      sx={{
+        width: 240, // Set your desired width
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: "25%", // Also set the width for the paper element
+          boxSizing: "border-box",
+        },
+      }}
+      onClose={toggleAiDrawer(false)}
+    >
+      <Toolbar />
+      <Typography
+        variant="h5"
+        gutterBottom
+        component="div"
+        sx={{ p: 2, pb: 0 }}
+      >
+        Personal Productivity Assistant
+      </Typography>
+      <Typography>
+        <Box>
+          {aiChat}
+        </Box>
+      </Typography>
+
     </Drawer>
   );
 }
+      
