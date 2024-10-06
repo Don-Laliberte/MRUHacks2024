4cadbe536fc113e10fb214f812c309de1cd655e7
Donnietentoes <45013341+Don-Laliberte@users.noreply.github.com>
2024-10-06 02:58:58 -0600

    Merging New Version to Main (#2)
    - Added Basic Navbar and Drawer Menu on the Right Side
    
    - Implemented kanbanboard to task menu
    
    - Finished Ai Window
    
    - Kanban integrated!!!
    
    ---------
    
    Co-authored-by: KingNektus <danny912nguyen@gmail.com>

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import { Paper, Typography } from '@mui/material';
+
+const Card = ({ item, index, onClick }) => (
+  <Draggable key={item.id} draggableId={item.id} index={index}>
+    {(provided, snapshot) => (
+      <Paper
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        elevation={snapshot.isDragging ? 6 : 1}
+        sx={{
+          p: 3,
+          mb: 1,
+          backgroundColor: snapshot.isDragging ? 'action.selected' : 'background.paper',
+          border: 'thick double',
+          borderColor: snapshot.isDragging ? 'primary.main' : 'black',
+          '&:hover': {
+            backgroundColor: 'action.hover',
+            borderColor: 'primary.light',
+          },
+        }}
+        onClick={onClick}
+      >
+        <Typography variant="body3">{item.title}</Typography>
+      </Paper>
+    )}
+  </Draggable>
+);
+
+export default Card;
